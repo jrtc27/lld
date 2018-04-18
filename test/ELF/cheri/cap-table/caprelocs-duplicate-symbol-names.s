jrtc27/lld

commit ef511486ce574a0032bbf9bd41d5c8be6c93d01f

Use R_MIPS_CHERI_CAPABILITY relocations for cap-table entries as well
Previously lld was always emitting __cap_relocs for the cap-table entries,
now it looks at the `-preemptible-caprelocs=` flags.

ELF/Arch/Cheri.cpp
@@ -554,6 +554,11 @@ uint32_t CheriCapTableSection::getIndex(const Symbol &Sym) const {
 
 template <class ELFT>
 void CheriCapTableSection::assignValuesAndAddCapTableSymbols() {
+  // FIXME: we should not be hardcoding architecture specific relocation numbers
+  // here
+  assert(Config->EMachine == EM_MIPS);
+  RelType ElfCapabilityReloc = R_MIPS_CHERI_CAPABILITY;
+
   uint64_t SmallEntryCount = 0;
   for (auto &it : Entries) {
     // TODO: looping twice is inefficient, we could keep track of the number of
@@ -592,10 +597,7 @@ void CheriCapTableSection::assignValuesAndAddCapTableSymbols() {
     }
 
     uint32_t Index = *CTI.Index;
-    auto Body = it.first;
-    In<ELFT>::CapRelocs->addCapReloc(
-        {InX::CheriCapTable, Index * Config->CapabilitySize, Config->Pic},
-        {Body, 0u}, Body->IsPreemptible, 0);
+    Symbol *TargetSym = it.first;
 
     StringRef Name = it.first->getName();
     if (Name.empty())
@@ -619,6 +621,10 @@ void CheriCapTableSection::assignValuesAndAddCapTableSymbols() {
     uint64_t Off = Index * Config->CapabilitySize;
     Symtab->addRegular(Saver.save(RefName), STV_HIDDEN, STT_OBJECT, Off,
                        Config->CapabilitySize, STB_LOCAL, this, nullptr);
+    addCapabilityRelocation<ELFT>(
+        *TargetSym, ElfCapabilityReloc, InX::CheriCapTable, Off,
+        R_CHERI_CAPABILITY, 0,
+        [&]() { return "\n>>> referenced by " + RefName; });
   }
   assert(AssignedSmallIndexes + AssignedLargeIndexes == Entries.size());
   ValuesAssigned = true;

test/ELF/cheri/cap-table/cap-table-init-relocs.s
@@ -1,0 +1,104 @@
+# RUN: %cheri128_purecap_llvm-mc -filetype=obj %s -o %t.o
+# RUN: ld.lld -shared -o %t %t.o -preemptible-caprelocs=legacy -local-caprelocs=legacy
+# RUN: llvm-readobj -r %t | FileCheck %s -check-prefix LEGACY-BOTH-ELF-RELOCS
+# RUN: llvm-objdump -C -h -t %t | FileCheck %s -check-prefix LEGACY-BOTH-CAPRELOCS
+# RUN: ld.lld -shared -o %t %t.o -preemptible-caprelocs=elf -local-caprelocs=legacy
+# RUN: llvm-readobj -r %t | FileCheck %s -check-prefix ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS
+# RUN: llvm-objdump -C -h -t %t | FileCheck %s -check-prefix ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS
+
+# TODO: ld.lld -o %t %t -preemptible-caprelocs=elf -local-caprelocs=cbuildcap
+
+# Not sure it makes sense to maintain the -local-caprelocs=elf codepath
+# RUN: ld.lld -shared -o %t %t.o -preemptible-caprelocs=elf -local-caprelocs=elf
+# RUN: llvm-readobj -r %t | FileCheck %s -check-prefix ELF-BOTH-ELF-RELOCS
+# RUN: llvm-objdump -C -h -t %t | FileCheck %s -check-prefix ELF-BOTH-CAPRELOCS
+
+.text
+.ent __start
+.global __start
+__start:
+clcbi $c1, %captab20(preemptible_var)($c26)
+clcbi $c2, %captab20(local_var)($c26)
+.end __start
+.data
+
+.global preemptible_var
+preemptible_var:
+.8byte 0x1234
+.size preemptible_var, 8
+
+
+.hidden local_var
+local_var:
+.8byte 0x5678
+.size local_var, 8
+
+
+# LEGACY-BOTH-CAPRELOCS-LABEL: CAPABILITY RELOCATION RECORDS:
+# LEGACY-BOTH-CAPRELOCS-NEXT: 0x0000000000030000	Base:  (0x0000000000000000)	Offset: 0x0000000000000000	Length: 0x0000000000000000	Permissions: 0x00000000
+# LEGACY-BOTH-CAPRELOCS-NEXT: 0x0000000000030010	Base: local_var (0x0000000000020008)	Offset: 0x0000000000000000	Length: 0x0000000000000008	Permissions: 0x00000000{{$}}
+# LEGACY-BOTH-CAPRELOCS-SAME: {{[[:space:]]$}}
+# LEGACY-BOTH-CAPRELOCS-LABEL: Sections:
+# LEGACY-BOTH-CAPRELOCS: 11 .cap_table    00000020 0000000000030000 DATA
+# LEGACY-BOTH-CAPRELOCS: 12 __cap_relocs  00000050 0000000000030020 DATA
+# LEGACY-BOTH-CAPRELOCS-LABEL: SYMBOL TABLE:
+# LEGACY-BOTH-CAPRELOCS:       0000000000030000 l       .cap_table              00000010 preemptible_var@CAPTABLE
+# LEGACY-BOTH-CAPRELOCS:       0000000000030010 l       .cap_table              00000010 local_var@CAPTABLE
+
+# two relative relocs for the location and one for the local var base. And then the ABSPTR/SIZE for the preemptible one
+# LEGACY-BOTH-ELF-RELOCS:      Relocations [
+# LEGACY-BOTH-ELF-RELOCS-NEXT:   Section (7) .rel.dyn {
+# LEGACY-BOTH-ELF-RELOCS-NEXT:     0x30020 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+# LEGACY-BOTH-ELF-RELOCS-NEXT:     0x30048 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+# LEGACY-BOTH-ELF-RELOCS-NEXT:     0x30050 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+# LEGACY-BOTH-ELF-RELOCS-NEXT:     0x30028 R_MIPS_CHERI_ABSPTR/R_MIPS_64/R_MIPS_NONE preemptible_var 0x0 (real addend unknown)
+# LEGACY-BOTH-ELF-RELOCS-NEXT:     0x30038 R_MIPS_CHERI_SIZE/R_MIPS_64/R_MIPS_NONE preemptible_var 0x0 (real addend unknown)
+# LEGACY-BOTH-ELF-RELOCS-NEXT:   }
+# LEGACY-BOTH-ELF-RELOCS-NEXT: ]
+
+
+
+
+
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS-LABEL: CAPABILITY RELOCATION RECORDS:
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS-NEXT: 0x0000000000030010	Base: local_var (0x0000000000020008)	Offset: 0x0000000000000000	Length: 0x0000000000000008	Permissions: 0x00000000{{$}}
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS-SAME: {{[[:space:]]$}}
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS-LABEL: Sections:
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS: 11 .cap_table    00000020 0000000000030000 DATA
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS: 12 __cap_relocs  00000028 0000000000030020 DATA
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS-LABEL: SYMBOL TABLE:
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS:       0000000000030000 l       .cap_table              00000010 preemptible_var@CAPTABLE
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-CAPRELOCS:       0000000000030010 l       .cap_table              00000010 local_var@CAPTABLE
+
+# One relative reloc for the local __cap_reloc location and one R_MIPS_CHERI_CAPABILITY for the preemptible symbol
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS:      Relocations [
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS-NEXT:   Section (7) .rel.dyn {
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS-NEXT:     0x30020 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+#                                                     ^----- location field in __cap_reloc[0]
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS-NEXT:     0x30028 R_MIPS_REL32/R_MIPS_64/R_MIPS_NONE - 0x0 (real addend unknown)
+#                                                     ^----- base field in __cap_reloc[0]
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS-NEXT:     0x30000 R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE preemptible_var 0x0 (real addend unknown)
+#                                                       ^---- preemptible_var@CAPTABLE
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS-NEXT:   }
+# ELF-PREEMPTIBLE-LEGACY-LOCAL-ELF-RELOCS-NEXT: ]
+
+
+# Finally if we use ELF relocs for local symbols as well we have no __cap_relocs, only two elf relocs:
+# ELF-BOTH-CAPRELOCS: CAPABILITY RELOCATION RECORDS:{{$}}
+# ELF-BOTH-CAPRELOCS-SAME: {{[[:space:]]$}}
+# ELF-BOTH-CAPRELOCS-LABEL: Sections:
+# ELF-BOTH-CAPRELOCS-NOT: __cap_relocs
+# ELF-BOTH-CAPRELOCS: 11 .cap_table    00000020 0000000000030000 DATA
+# ELF-BOTH-CAPRELOCS-NOT: __cap_relocs
+# ELF-BOTH-CAPRELOCS-LABEL: SYMBOL TABLE:
+# ELF-BOTH-CAPRELOCS:       0000000000030000 l       .cap_table              00000010 preemptible_var@CAPTABLE
+# ELF-BOTH-CAPRELOCS:       0000000000030010 l       .cap_table              00000010 local_var@CAPTABLE
+
+# ELF-BOTH-ELF-RELOCS:      Relocations [
+# ELF-BOTH-ELF-RELOCS-NEXT:   Section (7) .rel.dyn {
+# ELF-BOTH-ELF-RELOCS-NEXT:     0x30000 R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE preemptible_var 0x0 (real addend unknown)
+#                                   ^---- preemptible_var@CAPTABLE
+# ELF-BOTH-ELF-RELOCS-NEXT:     0x30010 R_MIPS_CHERI_CAPABILITY/R_MIPS_NONE/R_MIPS_NONE local_var 0x0 (real addend unknown)
+#                                    ^---- local_var@CAPTABLE
+# ELF-BOTH-ELF-RELOCS-NEXT:   }
+# ELF-BOTH-ELF-RELOCS-NEXT: ]

commit 1beb2cbca7afba86e86b004fd38a806b1a3bcb7e

Don't drop capability relocations for captable references to .L.foo symbols
This fixes crashes when accessing globals introduced in ef51148

Fixes #19

ELF/Arch/Cheri.cpp
@@ -595,24 +595,24 @@ void CheriCapTableSection::assignValuesAndAddCapTableSymbols() {
     uint32_t Index = *CTI.Index;
     Symbol *TargetSym = it.first;
 
-    StringRef Name = it.first->getName();
-    if (Name.empty())
-      continue;
-    // TODO: don't add for local symbols? or somehow rename?
-    // if (it.first->isLocal())
-    //   continue;s
-
-
+    StringRef Name = TargetSym->getName();
     // Avoid duplicate symbol name errors for unnamed string constants:
-    // XXXAR: maybe renumber them instead?
-    if (Name.startswith(".L.str"))
-      continue;
-    // XXXAR: for some reason we sometimes create more than one cap table entry
-    // for a given global name, for now just rename the symbol
-    // Could possibly happen with local symbols?
-    std::string RefName = (Name + "@CAPTABLE").str();
-    while (Symtab->find(RefName)) {
-      RefName += ".duplicate-name";
+    std::string RefName;
+    // Asumme any name with .L is a local name that may not be unique (e.g.
+    // string constants start with .L.strNNNN)
+    if (Name.empty() || Name.startswith(".L"))
+      RefName = (Name + "@CAPTABLE." + Twine(Index)).str();
+    else
+      RefName = (Name + "@CAPTABLE").str();
+
+    if (Symtab->find(RefName)) {
+      assert(TargetSym->isLocal());
+      // XXXAR: for some reason we sometimes create more than one cap table entry
+      // for a given global name, for now just rename the symbol
+      message("Found duplicate captable name " + RefName);
+      RefName = (Name + "@CAPTABLE." + Twine(Index)).str();
+      message(">>>Replacing with " + RefName);
+      assert(!Symtab->find(RefName) && "RefName should be unique");
     }
     uint64_t Off = Index * Config->CapabilitySize;
     Symtab->addRegular(Saver.save(RefName), STV_HIDDEN, STT_OBJECT, Off,

test/ELF/cheri/cap-table/caprelocs-duplicate-symbol-names.s
@@ -1,0 +1,76 @@
+# RUN: %cheri_purecap_llvm-mc %s -defsym=FILE=1 -filetype=obj -o %t1.o
+# RUN: %cheri_purecap_llvm-mc %s -defsym=FILE=2 -filetype=obj -o %t2.o
+# RUN: %cheri_purecap_llvm-mc %s -defsym=FILE=3 -filetype=obj -o %t-duplicate.o
+# Should not be possible to have duplicate names for globals in .captable
+# RUN: not ld.lld -shared %t1.o %t2.o %t-duplicate.o -o %t.so 2>&1 | FileCheck %s -check-prefix ERR
+# ERR: error: duplicate symbol: duplicate_global
+# ERR-NEXT: >>> defined at {{.+}}caprelocs-duplicate-symbol-names.s.tmp1.o:(duplicate_global)
+# ERR-NEXT: >>> defined at {{.+}}caprelocs-duplicate-symbol-names.s.tmp-duplicate.o:(.data+0x0)
+
+# But duplicate locals are fine so we need to add sensible @CAPTABLE names:
+# RUN: ld.lld -shared -preemptible-caprelocs=legacy %t1.o %t2.o -o %t.so
+# RUN: llvm-objdump -C -d -r -t %t.so | FileCheck %s
+
+# Check that we still have all the capability relocations:
+# See https://github.com/CTSRD-CHERI/lld/issues/19
+
+# CHECK: CAPABILITY RELOCATION RECORDS:
+# CHECK-NEXT: 0x0000000000030000	Base: .L.str.123 (0x0000000000020000)	Offset: 0x0000000000000000	Length: 0x0000000000000006	Permissions: 0x00000000
+# CHECK-NEXT: 0x0000000000030010	Base: .L.duplicate_local (0x0000000000020006)	Offset: 0x0000000000000000	Length: 0x0000000000000008	Permissions: 0x00000000
+# CHECK-NEXT: 0x0000000000030020	Base:  (0x0000000000000000)	Offset: 0x0000000000000000	Length: 0x0000000000000000	Permissions: 0x00000000
+# CHECK-NEXT: 0x0000000000030030	Base: .L.duplicate_local (0x0000000000020020)	Offset: 0x0000000000000000	Length: 0x0000000000000008	Permissions: 0x00000000
+# CHECK-NEXT: 0x0000000000030040	Base:  (0x0000000000000000)	Offset: 0x0000000000000000	Length: 0x0000000000000000	Permissions: 0x00000000
+
+
+# CHECK: SYMBOL TABLE:
+# CHECK: 0000000000030000         .cap_table		 00000050 _CHERI_CAPABILITY_TABLE_
+# CHECK: 0000000000030000 l       .cap_table		 00000010 .L.str.123@CAPTABLE.0
+# CHECK: 0000000000030010 l       .cap_table		 00000010 .L.duplicate_local@CAPTABLE.1
+# CHECK: 0000000000030020 l       .cap_table		 00000010 duplicate_global@CAPTABLE
+# CHECK: 0000000000030030 l       .cap_table		 00000010 .L.duplicate_local@CAPTABLE.3
+# CHECK: 0000000000030040 l       .cap_table		 00000010 g@CAPTABLE
+
+.macro add_captable_reference name
+.text
+clcbi $c1, %captab20(\name)($c1)  # add a reference so that it ends up in the captable
+.data
+.endm
+
+.macro add_data_symbol name, value:vararg
+.data
+\name:
+\value
+\name\().end:
+.size \name, \name\().end - \name
+
+.endm
+
+
+.if FILE == 1
+add_data_symbol .L.str.123, .asciiz "asdfg"
+add_captable_reference .L.str.123
+
+use_local_1:
+# Try a duplicate local name:
+add_data_symbol .L.duplicate_local, .quad 8
+add_captable_reference .L.duplicate_local
+
+.global duplicate_global
+add_data_symbol duplicate_global, .quad 10
+add_captable_reference duplicate_global
+.endif
+
+.if FILE == 2
+use_local_2:
+add_data_symbol .L.duplicate_local, .quad 9
+add_captable_reference .L.duplicate_local
+.global g
+add_data_symbol g, .quad 10
+add_captable_reference g
+.endif
+
+.if FILE == 3
+.global duplicate_global
+add_data_symbol duplicate_global, .quad 10
+add_captable_reference duplicate_global
+.endif